b3c8545ac7ae88ce5f5fd309ab2b18b875d51990
Shijil CG <shijilcg@gmail.com>
2025-11-17 22:15:06 +0530

    fix: remove duplicate copyright notice in footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <footer className="border-t border-gray-200 dark:border-gray-800 bg-gray-50 dark:bg-gray-900 mt-16">
         <div className="container mx-auto px-4 py-8 text-center text-gray-600 dark:text-gray-400">
           <p>&copy; 2025 Chembarathi. All rights reserved.</p>
+          <p>&copy; 2025 Chembarathi. All rights reserved.</p>
         </div>
       </footer>
     </>
