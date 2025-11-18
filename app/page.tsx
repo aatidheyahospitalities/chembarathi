92093552abae5d92226c0748833da47f95850867
Shijil CG <shijilcg@gmail.com>
2025-11-18 17:57:03 +0530

    refactor: reorganize contentful client and service functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getHeroSection } from "@/lib/services/getHeroSection";
+import { getHeroSection } from "./lib/services/getHeroSection";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { Document } from "@contentful/rich-text-types";
 
