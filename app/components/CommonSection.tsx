55e3a68451cdf9dac7b4ac65f1d723fb4e70c862
Shijil CG <shijilcg@gmail.com>
2025-11-26 23:04:28 +0530

    Refactor CommonSection to accept section type and update layout; add MarqueeSection component and integrate framer-motion for animations; enhance typography styles with responsive padding

--- app/components/CommonSection.tsx
+++ app/components/CommonSection.tsx
@@ -2,8 +2,9 @@ import { commonSectionData } from "../lib/type";
 import CommonLinkButton from "./CommonLinkButton";
 import Image from "next/image";
 
-export default async function commonSection({commonSectionData}: {commonSectionData: commonSectionData}) {
+export default async function commonSection({commonSectionData,type}: {commonSectionData: commonSectionData, type: 'TYPE1' | 'TYPE2' }) {
 
+const typeOfSection = type || "TYPE1";
 const title = commonSectionData?.fields.title || "";
 const description = commonSectionData?.fields.description || "";
 const ctaLabel = commonSectionData?.fields.ctaLabel || "";
@@ -12,18 +13,18 @@ const eyebrow = commonSectionData?.fields.eyebrow || "";
 const image = `https:${commonSectionData?.fields.image?.fields?.file?.url || ""}`;
 
   return (
-    <div className="p-huge-x gap-(--spacing-padding-16x) flex flex-col" >
-        <div className="flex gap-(--spacing-padding-16x)">
-            <div className="flex flex-col gap-(--spacing-padding-3x) w-[50%]">
+    <div className={`section-wrapper gap-(--spacing-padding-16x) flex flex-col ${typeOfSection === "TYPE1" ? "" : "flex-row"}`} >
+        <div className={`flex gap-(--spacing-padding-16x) ${typeOfSection === "TYPE1" ? "w-full" : "flex-col w-[50%]"}`}>
+            <div className={`flex flex-col gap-(--spacing-padding-3x) w-[50%] ${typeOfSection === "TYPE1" ? "w-[50%]" : "flex-col w-full"}`}>
               <span className="text-md-regular text-(--typography-color-secondary-500)">{eyebrow}</span>
               <h2 className="text-(--typography-color-secondary-100)">{title}</h2>
             </div>
-            <div className="flex flex-col gap-(--spacing-padding-10x) w-[50%] text-start">
+            <div className={`flex flex-col gap-(--spacing-padding-10x) w-[50%] text-start ${typeOfSection === "TYPE1" ? "w-[50%]" : "flex-col w-full"}`}>
               <span className="text-xl-regular text-(--typography-color-secondary-800)">{description}</span>
-              <CommonLinkButton text={ctaLabel}  />
+              <CommonLinkButton text={ctaLabel}  /> 
             </div>
         </div>
-        <div className="w-full h-[702px] flex relative">
+        <div className={` h-[702px] flex relative ${typeOfSection === "TYPE1" ? "w-full" : "w-[50%]"}`}>
           <Image src={image} alt="Common Section Image" className="object-cover" fill loading="eager"/>
         </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import CommonSection from "./components/CommonSection";
 
 import { getContent, getMetaData } from "./lib/services/getHomeContent";
 import { HeroData, commonSectionData } from "./lib/type";
+import MarqueeSection from "./components/InfiniteScrollText";
+
+export const revalidate = 600; // Revalidate every 10 minutes
 
 export async function generateMetadata() {
   const metaData = await getMetaData();
@@ -17,11 +20,15 @@ export default async function HomePage() {
 
   const data = await getContent();
   const heroData: HeroData = data.items[0]?.fields.hero as HeroData || {};
-  const commonSectionData: commonSectionData = data.items[0]?.fields.twoColumnShowcaseSection as unknown as commonSectionData || {};
+  const twoColumnSection = data.items[0]?.fields.twoColumnShowcaseSection as unknown[];
+  const AboutData: commonSectionData = (twoColumnSection?.[0] as unknown as commonSectionData) || {};
+  const ExperienceData: commonSectionData = (twoColumnSection?.[1] as unknown as commonSectionData) || {};
   return (
     <div>
       <Banner heroData={heroData} />
-      <CommonSection commonSectionData={commonSectionData} />
+      <CommonSection commonSectionData={AboutData} type="TYPE1" />
+      <CommonSection commonSectionData={ExperienceData} type="TYPE2" />
+      <MarqueeSection/>
     </div>
   );
 }
