7797bb97018d219f1ec2ac20436c1cbe01a3cda2
Shijil CG <shijilcg@gmail.com>
2025-12-05 21:27:22 +0530

    fix: remove redundant stylesheet link for Material Symbols

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,7 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=north_east"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=star"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </head>
       <body>
