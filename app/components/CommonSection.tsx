0ed839dd9a5eaa1133911e0373f6f460908e5c11
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-12-07 09:03:22 +0530

    fix: style

--- app/components/CommonSection.tsx
+++ app/components/CommonSection.tsx
@@ -18,13 +18,19 @@ export default async function commonSection({
 
   return (
     <div
-      className={`section-wrapper gap-(--spacing-padding-16x) flex flex-col ${typeOfSection === 'TYPE1' ? '' : 'flex-row'}`}
+      className={`section-wrapper gap-(--spacing-padding-16x) flex flex-col ${
+        typeOfSection === 'TYPE1' ? '' : 'flex-row'
+      }`}
     >
       <div
-        className={`flex gap-(--spacing-padding-16x) ${typeOfSection === 'TYPE1' ? 'w-full' : 'flex-col w-[50%]'}`}
+        className={`flex gap-(--spacing-padding-16x) ${
+          typeOfSection === 'TYPE1' ? 'w-full' : 'flex-col w-[50%]'
+        }`}
       >
         <div
-          className={`flex flex-col gap-(--spacing-padding-3x) w-[50%] ${typeOfSection === 'TYPE1' ? 'w-[50%]' : 'flex-col w-full'}`}
+          className={`flex flex-col gap-(--spacing-padding-3x) w-[50%] ${
+            typeOfSection === 'TYPE1' ? 'w-[50%]' : 'flex-col w-full'
+          }`}
         >
           <span className="text-md-regular text-(--typography-color-secondary-500)">
             {eyebrow}
@@ -32,7 +38,9 @@ export default async function commonSection({
           <h2 className="text-(--typography-color-secondary-100)">{title}</h2>
         </div>
         <div
-          className={`flex flex-col gap-(--spacing-padding-10x) w-[50%] text-start ${typeOfSection === 'TYPE1' ? 'w-[50%]' : 'flex-col w-full'}`}
+          className={`flex flex-col gap-(--spacing-padding-10x) w-[50%] text-start ${
+            typeOfSection === 'TYPE1' ? 'w-[50%]' : 'flex-col w-full'
+          }`}
         >
           <span className="text-xl-regular text-(--typography-color-secondary-800)">
             {description}
@@ -41,7 +49,9 @@ export default async function commonSection({
         </div>
       </div>
       <div
-        className={`flex relative aspect-video ${typeOfSection === 'TYPE1' ? 'w-full' : 'w-[50%]'}`}
+        className={`flex relative aspect-video rounded-[32px] overflow-hidden ${
+          typeOfSection === 'TYPE1' ? 'w-full' : 'w-[50%]'
+        }`}
       >
         <Image
           src={image}
@@ -49,6 +59,7 @@ export default async function commonSection({
           className="object-cover"
           fill
           loading="eager"
+          quality={100}
         />
       </div>
     </div>
