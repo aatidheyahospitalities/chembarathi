22e0511fab17c3855e6429fa97a5093d11a4c4e0
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-12-08 08:31:09 +0530

    update style for button

--- app/components/CommonLinkButton.tsx
+++ app/components/CommonLinkButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-export default function commonLinkButton({
+export default function CommonLinkButton({
   text,
   onclick,
 }: {
@@ -13,7 +13,10 @@ export default function commonLinkButton({
       className="group relative w-fit py-3 flex gap-2 items-center outline-none border-b-2 border-(--border-color-default) cursor-pointer"
     >
       <div className="relative flex items-start">
-        <h6 className="text-(--typography-color-secondary-100)">{text}</h6>
+        {/* Button Text in span */}
+        <span className="text-xl-regular text-(--typography-color-secondary-100)">
+          {text}
+        </span>
 
         {/* arrow */}
         <span className="absolute text-base text-white transition-transform duration-300 ease-in-out material-symbols-outlined -right-5 -top-1 group-hover:translate-x-1 group-hover:-translate-y-1 group-active:translate-y-0">
