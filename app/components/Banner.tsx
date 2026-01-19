360d9a4d2e3a73a25dca43912cff17bfd5df0b8f
rishiyee <hrishikeshvyshnavop@gmail.com>
2026-01-19 22:44:29 +0530

    Fix: Remove the hero description so only the title is displayed.

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,24 +1,23 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { HeroType } from '../lib/type';
 
 export default function Banner({ heroData }: Readonly<{ heroData: HeroType }>) {
   const heroTitle = heroData?.title || '';
   const heroImage = heroData?.image.url || '';
-  const heroDescription = heroData?.description.json || null;
 
   return (
     <div
-      className="relative flex items-end justify-center min-h-screen bg-center bg-no-repeat bg-cover banner-wrapper"
+      className="relative flex items-center justify-center min-h-screen bg-center bg-no-repeat bg-cover banner-wrapper"
       style={{ backgroundImage: `url(${heroImage})` }}
     >
-      {/* Overlay for better text readability */}
+      {/* Overlay */}
       <div className="absolute inset-0 bg-black/40"></div>
+
       {/* Content */}
-      <div className="relative z-10 flex w-full text-white justify-space-between gap-(--spacing-padding-16x) xs:flex-col xs:gap-(--spacing-padding-4x)">
-        <h1 className="w-[50%] xs:!text-h4 xs:w-full">{heroTitle}</h1>
-        <div className="text-xl-regular w-[50%] text-(--typography-color-secondary-600) xs:!text-body-lg xs:w-full">
-          {heroDescription && documentToReactComponents(heroDescription)}
-        </div>
+      <div className="relative z-10 flex w-full text-white justify-center">
+      <h1 className="text-center xs:!text-h4 w-[500px]">
+  {heroTitle}
+</h1>
+
       </div>
     </div>
   );
