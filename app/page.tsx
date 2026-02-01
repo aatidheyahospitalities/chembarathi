e5f4d9db1889e7bb87a18651873bf02980d73141
rishiyee <hrishikeshvyshnavop@gmail.com>
2026-02-01 23:08:53 +0530

    Enhance: reintroduce GalleryLoop component in HomePage and refactor to use state for image ratios

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default async function HomePage() {
 
       <ReviewSection />
       <FaqSection {...faqData} />
-      {/* <GalleryLoop /> */}
+      <GalleryLoop />
       <BottomBarSection />
     </main>
   );
